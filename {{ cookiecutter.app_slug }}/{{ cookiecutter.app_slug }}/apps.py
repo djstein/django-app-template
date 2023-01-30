bc560b3331080035a818dd5ca8d6844e2ae998de
Dylan Stein <djstein@ncsu.edu>
2023-01-29 19:14:05 -0800

    add cookiecutter to app_slug

--- {{ cookiecutter.app_slug }}/{{ cookiecutter.app_slug }}/apps.py
+++ {{ cookiecutter.app_slug }}/{{ cookiecutter.app_slug }}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{ app_slug|capitalize }}Config(AppConfig):
+class {{ cookiecutter.app_slug|capitalize }}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "{{ cookiecutter.app_slug }}"
