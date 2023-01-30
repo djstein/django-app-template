2e6676ab44f1cdf75acc7ea60f0d4ddb76b2d014
Dylan Stein <djstein@ncsu.edu>
2023-01-29 19:13:17 -0800

    app slug

--- {{ cookiecutter.app_slug }}/{{ cookiecutter.app_slug }}/apps.py
+++ {{ cookiecutter.app_slug }}/{{ cookiecutter.app_slug }}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{ project_slug|capitalize }}Config(AppConfig):
+class {{ app_slug|capitalize }}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "{{ cookiecutter.app_slug }}"
